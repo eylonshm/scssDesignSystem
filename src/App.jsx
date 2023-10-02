322d3b8ddc6047ae0ea1e8c2c3674e660e05f01f
Eylon Shmilovich <eylonshm@gmail.com>
2023-10-02 16:13:59 +0300

    Project init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import '../styles/_index.scss'
+import WorkBox from './components/WorkBox'
+
+function App() {
+  return (
+    <>
+      <WorkBox />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/WorkBox/index.jsx
@@ -0,0 +1,13 @@
+import './index.scss'
+
+const WorkBox = () => {
+  return (
+    <div class=' '>
+      <h2 class='pTag br-s mb-l fs-7'>App is running.</h2>
+      <p class=''>You can see this text just on desktop</p>
+      <button class='button'>Click here</button>
+    </div>
+  )
+}
+
+export default WorkBox
