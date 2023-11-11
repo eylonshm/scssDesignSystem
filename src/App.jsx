cf0ddc71bc0901a5f69562242a3012ff2e85ab87
Eylon Shmilovich <eylonshm@gmail.com>
2023-11-11 19:57:10 +0200

    Add ReactMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,52 @@
-import './App.css'
-import '../styles/_index.scss'
-import WorkBox from './components/WorkBox'
+import React from "react";
+import "./App.css";
+import "../styles/_index.scss";
+import WorkBox from "./components/WorkBox";
+import { useCallback, useState } from "react";
+
+// eslint-disable-next-line react/prop-types
+const AnotherComponent = ({ onClick }) => {
+  console.log("render");
+  return (
+    <div>
+      {console.dir(onClick)}
+      <p>Text</p>
+      <p>Another text</p>
+      <button onClick={onClick}>Dec</button>
+    </div>
+  );
+};
+
+const MemoizedAnotherComp = React.memo(
+  AnotherComponent,
+  (prevProps, nextProps) => {
+    if (prevProps.onClick === nextProps.onClick) {
+      return true; // props are equal
+    }
+    return false; // props are not equal -> update the component
+  }
+);
 
 function App() {
+  const [count, setCount] = useState(0);
+  const increment = () => {
+    // eslint-disable-next-line no-debugger
+    setCount((count) => count + 1);
+    // eslint-disable-next-line no-debugger
+  };
+
+  const dec = useCallback(() => {
+    if (count === 0) return;
+    setCount((count) => count - 1);
+  }, [count]);
+
   return (
     <>
-      <WorkBox />
+      <WorkBox onClick={increment} />
+      <p>Count: {count}</p>
+      <MemoizedAnotherComp onClick={dec} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/WorkBox/index.jsx
+++ src/components/WorkBox/index.jsx
@@ -1,11 +1,14 @@
 import "./index.scss";
 
-const WorkBox = () => {
+// eslint-disable-next-line react/prop-types
+const WorkBox = ({ onClick }) => {
   return (
     <div className=" ">
       <h2 className="pTag br-s mb-l fs-7">App is running.</h2>
       <p className="">You can see this text just on desktop</p>
-      <button className="button">Click here</button>
+      <button className="button" onClick={onClick}>
+        Click here
+      </button>
     </div>
   );
 };
