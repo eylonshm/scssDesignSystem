d0ce24898e35134af263b9f4edcc2a0deb583711
Eylon Shmilovich <eylonshm@gmail.com>
2023-11-11 13:56:52 +0200

    Rename class atr to className

--- src/components/WorkBox/index.jsx
+++ src/components/WorkBox/index.jsx
@@ -1,13 +1,13 @@
-import './index.scss'
+import "./index.scss";
 
 const WorkBox = () => {
   return (
-    <div class=' '>
-      <h2 class='pTag br-s mb-l fs-7'>App is running.</h2>
-      <p class=''>You can see this text just on desktop</p>
-      <button class='button'>Click here</button>
+    <div className=" ">
+      <h2 className="pTag br-s mb-l fs-7">App is running.</h2>
+      <p className="">You can see this text just on desktop</p>
+      <button className="button">Click here</button>
     </div>
-  )
-}
+  );
+};
 
-export default WorkBox
+export default WorkBox;
