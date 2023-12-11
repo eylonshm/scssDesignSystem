1849fae6bbcedc2fc69c1b13f4d429bbb49b25c5
Eylon Shmilovich <eylonshm@gmail.com>
2023-12-11 22:19:59 +0200

    Make Cypress tests work as expected

--- cypress/e2e/counterIsWorking.cy.js
+++ cypress/e2e/counterIsWorking.cy.js
@@ -2,22 +2,26 @@
 /// <reference types="cypress" />
 
 describe("Counter is indeed working", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
   it("Count is exists and initialy set to zero", () => {
-    cy.visit("/s");
     cy.get('[data-cy="counter"]').should("be.visible");
     cy.get('[data-cy="counter"]').should("have.text", "Count: 0");
   });
   it("Increment button is clickable", () => {
-    cy.get('[data.cy="counter-inc-btn"]').should("be.visible").click();
+    cy.get('[data-cy="counter-inc-btn"]').should("be.visible").click();
     cy.get('[data-cy="counter"]').should("be.visible");
     cy.get('[data-cy="counter"]').should("have.text", "Count: 1");
   });
   it("Increment is working more than once", () => {
-    cy.get('[data-cy="counter-inc-btn"]').should("be.visible").click();
+    cy.get('[data-cy="counter-inc-btn"]').should("be.visible").click().click();
     cy.get('[data-cy="counter"]').should("have.text", "Count: 2");
   });
   it("Decrement button is clickable and changes count", () => {
-    cy.get('[data-cy="counter-dec-btn"]').should("be.visible").click();
+    cy.get('[data-cy="counter-inc-btn"]').should("be.visible").click();
     cy.get('[data-cy="counter"]').should("have.text", "Count: 1");
+    cy.get('[data-cy="counter-dec-btn"]').should("be.visible").click();
+    cy.get('[data-cy="counter"]').should("have.text", "Count: 0");
   });
 });
